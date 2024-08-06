83b97495fb00ee39c5b37d4a113cc0ecaaee3816
AshleyLleshi10 <100548548+AshleyLleshi10@users.noreply.github.com>
2024-08-06 14:27:01 +0930

    Update AzimuthMonitoring.hpp

--- week2/AzimuthMonitoring/AzimuthMonitoring.hpp
+++ week2/AzimuthMonitoring/AzimuthMonitoring.hpp
@@ -5,35 +5,35 @@ class AzimuthMonitoring {
     public:
         int getAzimuth (std::vector<std::string> instructions[]){
             int azimuth = 360;
-            for (int i = 0; i <= instructions->length(); i++)
+            for (int i = 0; i <= instructions->size(); i++)
             {
                 //std::cout << azimuth << std::endl;
-                if (instructions[i].length() == 5)
+                if (instructions[i].size() == 5)
                 {
                     //std::cout << "ping right" << std::endl;
                     azimuth = azimuth + 90;
                 }
-                else if (instructions[i].length() == 4 && instructions[i][0] == 'L')
+                else if (instructions[i].size() == 4 && instructions[i][0] == 'L')
                 {
                     //std::cout << "ping left" << std::endl;   
                     azimuth = azimuth - 90; 
                 }
-                else if (instructions[i].length() == 11)
+                else if (instructions[i].size() == 11)
                 {
                     //std::cout << "ping 180" << std::endl;   
                     azimuth = azimuth + 180;
                 }
-                else if (instructions[i].length() == 4 && instructions[i][0] == 'H')
+                else if (instructions[i].size() == 4 && instructions[i][0] == 'H')
                 {
                     //std::cout << "ping stop" << std::endl;   
                     break;
                 }
-                else if (instructions[i].length() >= 6 && instructions[i][0] == 'L')
+                else if (instructions[i].size() >= 6 && instructions[i][0] == 'L')
                 {
                     //std::cout << "ping left x" << std::endl;   
                     azimuth = azimuth - stoi(instructions[i].erase(0,5));
                 }
-                else if (instructions[i].length() >= 7 && instructions[i][0] == 'R')
+                else if (instructions[i].size() >= 7 && instructions[i][0] == 'R')
                 {
                     //std::cout << "ping right x" << std::endl;   
                     azimuth = azimuth + stoi(instructions[i].erase(0,6));
